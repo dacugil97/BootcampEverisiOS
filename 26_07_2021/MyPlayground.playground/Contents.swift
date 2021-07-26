2f7a810dabba5d1d000cf37bf562aa0094f5b56e
David Cuñado Gil <dcunadog@VLD-C02FV0Z0ML85.home>
2021-07-26 08:27:37 +0200

    [WIP] - My first commiton GitHub -

---
+++ 26_07_2021/MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+// MARK: - TIPOS BÁSICOS
+
+var nombre: String = "Pepe" // var -> Propiedad mutable
+let apellido: String = "Rodriguez"  //let -> propiedad inmutable
