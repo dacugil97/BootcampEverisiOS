bcf0231ab845d35c6a58bbd1b150718a100fbff7
David Cuñado Gil <dcunadog@VLD-C02FV0Z0ML85.home>
2021-07-26 12:47:20 +0200

    Fin primera sesion

--- 26_07_2021/AppHolaMundo/AppHolaMundo/ViewController.swift
+++ 26_07_2021/AppHolaMundo/AppHolaMundo/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var holaMundoLbl: UILabel!
+    @IBOutlet weak var saludoSolLbl: UILabel!
+    @IBOutlet weak var constPrimerLbl: NSLayoutConstraint!
+    
+    // MARK: - IBActions
+    
+    @IBAction func cambiaNombreBtn(_ sender: Any) {
+        
+        self.holaMundoLbl.text = "Hola bootcamp iOS Everis"
+        self.saludoSolLbl.text = "Hola Sol y hola Luna"
+        self.constPrimerLbl.constant = 50
+    }
+    
+    
     // MARK: - life VC
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
     }
 
